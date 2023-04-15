4751b838d25e9d7a67cb1a5ea868ca776c662f6f
Lee <43488305+poiu694@users.noreply.github.com>
2023-04-15 20:33:34 +0900

    feat: 사용되는 font 계층화

---
+++ src/theme/fonts.ts
@@ -0,0 +1,76 @@
+import { css } from '@emotion/react';
+
+export const fonts = {
+  h1: css`
+    font-weight: 800;
+    font-size: 48px;
+    letter-spacing: -0.015em;
+    line-height: 1.4;
+  `,
+  h2: css`
+    font-weight: 700;
+    font-size: 24px;
+    letter-spacing: -0.015em;
+    line-height: 1.4;
+  `,
+  h3: css`
+    font-weight: 700;
+    font-size: 20px;
+    letter-spacing: -0.015em;
+    line-height: 1.4;
+  `,
+  h4: css`
+    font-weight: 700;
+    font-size: 18px;
+    letter-spacing: -0.015em;
+    line-height: 1.4;
+  `,
+  h5: css`
+    font-weight: 700;
+    font-size: 16px;
+    letter-spacing: -0.015em;
+    line-height: 1.4;
+  `,
+  h6: css`
+    font-weight: 700;
+    font-size: 14px;
+    letter-spacing: -0.015em;
+    line-height: 1.4;
+  `,
+  h7: css`
+    font-weight: 700;
+    font-size: 12px;
+    letter-spacing: -0.015em;
+    line-height: 1.4;
+  `,
+  body1: css`
+    font-weight: 400;
+    font-size: 18px;
+    letter-spacing: -0.018em;
+    line-height: 1.6;
+  `,
+  body2: css`
+    font-weight: 400;
+    font-size: 16px;
+    letter-spacing: -0.018em;
+    line-height: 1.6;
+  `,
+  body3: css`
+    font-weight: 400;
+    font-size: 14px;
+    letter-spacing: -0.018em;
+    line-height: 1.6;
+  `,
+  body4: css`
+    font-weight: 400;
+    font-size: 12px;
+    letter-spacing: -0.018em;
+    line-height: 1.6;
+  `,
+  body5: css`
+    font-weight: 400;
+    font-size: 10px;
+    letter-spacing: -0.018em;
+    line-height: 1.6;
+  `,
+};
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,9 @@
 import { colors } from './colors';
+import { fonts } from './fonts';
 
 export const theme = {
   colors,
+  fonts,
 } as const;
 
 export type ThemeType = typeof theme;
