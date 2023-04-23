3f2f81dcc7984bd6c56a6f71c4cd98adbef4a073
Lee <43488305+poiu694@users.noreply.github.com>
2023-04-23 22:54:40 +0900

    feat: Text InfiniteScroll 컴포넌트 생성

---
+++ src/components/LandingScrollText.tsx
@@ -0,0 +1,61 @@
+import styled from '@emotion/styled';
+
+import { Text } from './common';
+import { keyframes, useTheme } from '@emotion/react';
+
+const SLOGAN = 'DISCOVER YOUR PERFECT SKIN WITH EGGY';
+
+interface Props {
+  row: number;
+}
+
+function LandingScrollText({ row }: Props) {
+  const theme = useTheme();
+  const texts = [...Array(row)].map(() => SLOGAN);
+
+  return (
+    <Wrapper>
+      {texts.map((text, idx) => (
+        <MarQuee key={idx} x={idx * 100}>
+          <AnimationText variant="h1" color={theme.colors.gray400}>
+            {text}
+          </AnimationText>
+          <AnimationText variant="h1" color={theme.colors.gray400}>
+            {text}
+          </AnimationText>
+        </MarQuee>
+      ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  max-width: 100%;
+  overflow: hidden;
+`;
+
+const RightToLeft = keyframes`
+  from {
+    transform: translateX(-30%);
+  }
+  to {
+    transform: translateX(-80%);
+  }
+`;
+
+type X = { x: number };
+const MarQuee = styled.div<X>`
+  white-space: nowrap;
+  overflow: hidden;
+  display: inline-block;
+  animation: ${RightToLeft} 8s linear infinite;
+  position: relative;
+  left: ${({ x }) => `${x}px`};
+`;
+
+const AnimationText = styled(Text)`
+  display: inline-block;
+  line-height: 1;
+`;
+
+export default LandingScrollText;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './baumann';
 export * from './common';
 export * from './icons';
 export * from './styled';
+export { default as LandingScrollText } from './LandingScrollText';
