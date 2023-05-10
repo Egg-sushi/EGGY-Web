eba166b96e10d0a2f5b3351126688596e32156e3
Lee <43488305+poiu694@users.noreply.github.com>
2023-05-10 14:37:08 +0900

    refactor: 중복되는 코드 개선

--- src/components/modal/ProductSearchFilter.tsx
+++ src/components/modal/ProductSearchFilter.tsx
@@ -3,6 +3,7 @@ import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { Flex } from '../styled';
+import { FilterHierarchy, inArray } from '@/utils';
 import { Button, Tag, Text } from '../common';
 import type { UserFilterList, ProductFilter } from '@/types/product';
 
@@ -14,8 +15,29 @@ interface Props {
   onSaveClose: (data: ProductFilter) => void;
 }
 
+function toTypeFilterList(list: Props['data']['list']) {
+  return [
+    {
+      type: 'categories',
+      title: 'Category',
+      items: list.categories,
+    },
+    {
+      type: 'skinTypes',
+      title: 'SkinType',
+      items: list.skinTypes,
+    },
+    {
+      type: 'priceRanges',
+      title: 'Price',
+      items: list.priceRanges,
+    },
+  ] as const;
+}
+
 function CosmeticSearchFilter({ data: { filters, list }, onSaveClose }: Props) {
   const [selectedFilters, setSelectedFilters] = React.useState<ProductFilter>(filters);
+  const typeList = toTypeFilterList(list);
   const theme = useTheme();
 
   const handleClickFilterItem = React.useCallback(
@@ -38,56 +60,28 @@ function CosmeticSearchFilter({ data: { filters, list }, onSaveClose }: Props) {
 
   return (
     <Wrapper flexDirection="column" gap={24}>
-      <Flex flexDirection="column" gap={8}>
-        <Text variant="h5" color={theme.colors.blue800}>
-          Category
-        </Text>
-        <Flex gap={4} flexWrap="wrap">
-          {list.categories.map((category) => (
-            <TransitionTag
-              size="md"
-              key={category.id}
-              text={category.title}
-              hierarchy={selectedFilters.categories.includes(category.title) ? 'skyblue' : 'gray'}
-              onClick={() => handleClickFilterItem('categories', category)}
-            />
-          ))}
+      {typeList.map((list) => (
+        <Flex flexDirection="column" gap={8} key={list.type}>
+          <Text variant="h5" fontColor={theme.colors.blue800}>
+            {list.title}
+          </Text>
+          <Flex gap={4} flexWrap="wrap">
+            {list.items.map((item) => (
+              <TransitionTag
+                size="md"
+                key={item.id}
+                text={item.title}
+                hierarchy={
+                  inArray(selectedFilters[list.type], item.title)
+                    ? FilterHierarchy[list.type]
+                    : 'gray'
+                }
+                onClick={() => handleClickFilterItem(list.type, item)}
+              />
+            ))}
+          </Flex>
         </Flex>
-      </Flex>
-      <Flex flexDirection="column" gap={8}>
-        <Text variant="h5" color={theme.colors.blue800}>
-          SkinTypes
-        </Text>
-        <Flex gap={4} flexWrap="wrap">
-          {list.skinTypes.map((skinType) => (
-            <TransitionTag
-              size="md"
-              key={skinType.id}
-              text={skinType.title}
-              hierarchy={selectedFilters.skinTypes.includes(skinType.title) ? 'skyblue' : 'gray'}
-              onClick={() => handleClickFilterItem('skinTypes', skinType)}
-            />
-          ))}
-        </Flex>
-      </Flex>
-      <Flex flexDirection="column" gap={8}>
-        <Text variant="h5" color={theme.colors.blue800}>
-          PriceRanges
-        </Text>
-        <Flex gap={4} flexWrap="wrap">
-          {list.priceRanges.map((priceRange) => (
-            <TransitionTag
-              size="md"
-              key={priceRange.id}
-              text={priceRange.title}
-              hierarchy={
-                selectedFilters.priceRanges.includes(priceRange.title) ? 'skyblue' : 'gray'
-              }
-              onClick={() => handleClickFilterItem('priceRanges', priceRange)}
-            />
-          ))}
-        </Flex>
-      </Flex>
+      ))}
       <Button
         variant="filled"
         hierarchy="primary"
