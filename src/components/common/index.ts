2bcc41069c697746daeb0a4cd777fb0cb3a48ce5
LeeMir <manofgodmj@gmail.com>
2023-05-11 20:07:18 +0900

    fix: ProgressBar export

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -5,5 +5,6 @@ export { default as Button } from './Button';
 export { default as Header } from './Header';
 export { default as SelectOption } from './SelectOption';
 export { default as SkeletonImage } from './SkeletonImage';
+export { default as ProgressBar } from './ProgressBar';
 export { default as CircleCheckBox } from './CircleCheckBox';
 export { default as GenderInputBox } from './GenderInputBox';
