458a2a822d865979571fb209f02327fc14d15959
Lee <43488305+poiu694@users.noreply.github.com>
2023-05-11 22:18:27 +0900

    feat: user 스킨타입 조회

--- src/api/service/UserService.ts
+++ src/api/service/UserService.ts
@@ -1,5 +1,6 @@
 import { SignUpInfo } from '@/types/api';
 import HTTPInterface from './core';
+import { SkinType } from '@/types/baumann';
 
 class UserService extends HTTPInterface {
   public constructor() {
@@ -11,6 +12,27 @@ class UserService extends HTTPInterface {
       .post('signup', {
         ...data,
       })
+      .then(HTTPInterface._handleResponse)
+      .catch(HTTPInterface._handleError);
+  }
+
+  public logOut(): Promise<{ message: string }[]> {
+    return this.baseHTTP
+      .post('logout')
+      .then(HTTPInterface._handleResponse)
+      .catch(HTTPInterface._handleError);
+  }
+
+  public isLogin(): Promise<{ isLogin: boolean }> {
+    return this.baseHTTP
+      .get('login/check')
+      .then(HTTPInterface._handleResponse)
+      .catch(HTTPInterface._handleError);
+  }
+
+  public getUserSkinType(): Promise<{ skinType?: SkinType }> {
+    return this.baseHTTP
+      .get('user/skintype')
       .then(HTTPInterface._handleResponse)
       .catch(HTTPInterface._handleError);
   }
